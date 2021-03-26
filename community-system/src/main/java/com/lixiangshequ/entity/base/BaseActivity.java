15b6b2f88b9b8f40bf94dfd620242832b9821552
ttx <1710643798@qq.com>
2021-03-26 13:57:57 +0800

    活动删除，批量删除，更新

--- community-system/src/main/java/com/lixiangshequ/entity/base/BaseActivity.java
+++ community-system/src/main/java/com/lixiangshequ/entity/base/BaseActivity.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -33,15 +35,23 @@ public class BaseActivity {
     private String organizer;
 
     @TableField("created")
+    //入参格式化器，拦截器中传入参数格式化为这种形式保存
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //出参格式化器，返回到前端的数据格式化为这种类型
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date created;
 
     @TableField("type")
     private Integer type;
 
     @TableField("beginTime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date beginTime;
 
     @TableField("endTime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 
     @TableField("hot")
